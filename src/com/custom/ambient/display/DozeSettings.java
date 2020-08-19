NusantaraProject-ROM/android_packages_apps_CustomDoze

CustomDoze: Allow to wake the screen instead of pulsing
 * Highly inspired by iOS raise to wake

Co-authored-by: Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
Co-authored-by: Anushek Prasal <anushekprasal@gmail.com>
Change-Id: I832fd03e9538a507ec9b3ee7cc384bd71d6efbdd
Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

src/com/custom/ambient/display/DozeSettings.java
@@ -68,6 +68,7 @@ public class DozeSettings extends PreferenceActivity implements PreferenceFragme
         private SwitchPreference mAoDPreference;
         private SwitchPreference mAmbientDisplayPreference;
         private SwitchPreference mPickUpPreference;
+        private SwitchPreference mRaiseToWakePreference;
         private SwitchPreference mHandwavePreference;
         private SwitchPreference mPocketPreference;
         private SystemSettingSwitchPreference mDozeOnChargePreference;
@@ -117,6 +118,11 @@ public class DozeSettings extends PreferenceActivity implements PreferenceFragme
             mPickUpPreference.setChecked(Utils.tiltGestureEnabled(mContext));
             mPickUpPreference.setOnPreferenceChangeListener(this);
 
+            mRaiseToWakePreference =
+                (SwitchPreference) findPreference(Utils.GESTURE_RAISE_TO_WAKE_KEY);
+            mRaiseToWakePreference.setChecked(Utils.isRaiseToWakeEnabled(mContext));
+            mRaiseToWakePreference.setOnPreferenceChangeListener(this);
+
             mHandwavePreference =
                 (SwitchPreference) findPreference(Utils.GESTURE_HAND_WAVE_KEY);
             mHandwavePreference.setChecked(Utils.handwaveGestureEnabled(mContext));
@@ -149,6 +155,7 @@ public class DozeSettings extends PreferenceActivity implements PreferenceFragme
 
             if (!getResources().getBoolean(R.bool.has_tilt_sensor)) {
                 mPickUpPreference.setVisible(false);
+                mRaiseToWakePreference.setVisible(false);
             }
 
             if (!getResources().getBoolean(R.bool.has_proximity_sensor)) {
@@ -179,6 +186,11 @@ public class DozeSettings extends PreferenceActivity implements PreferenceFragme
                 boolean value = (Boolean) newValue;
                 mPickUpPreference.setChecked(value);
                 Utils.enablePickUp(value, mContext);
+                return true;
+            } else if (Utils.GESTURE_RAISE_TO_WAKE_KEY.equals(key)) {
+                boolean value = (Boolean) newValue;
+                mRaiseToWakePreference.setChecked(value);
+                Utils.enableRaiseToWake(value, mContext);
                 return true;
             } else if (Utils.GESTURE_HAND_WAVE_KEY.equals(key)) {
                 boolean value = (Boolean) newValue;
@@ -216,6 +228,7 @@ public class DozeSettings extends PreferenceActivity implements PreferenceFragme
             final boolean aodChargeEnabled = Utils.isAoDChargeEnabled(mContext);
             mAmbientDisplayPreference.setEnabled(!aodEnabled);
             mPickUpPreference.setEnabled(!aodEnabled);
+            mRaiseToWakePreference.setEnabled(!aodEnabled);
             mHandwavePreference.setEnabled(!aodEnabled);
             mPocketPreference.setEnabled(!aodEnabled);
             mDozeOnChargePreference.setEnabled(!aodEnabled);

src/com/custom/ambient/display/Utils.java
@@ -39,6 +39,7 @@ public final class Utils {
     protected static final String AOD_CHARGE_KEY = "doze_on_charge";
     protected static final String AMBIENT_DISPLAY_KEY = "ambient_display";
     protected static final String PICK_UP_KEY = "pick_up";
+    protected static final String GESTURE_RAISE_TO_WAKE_KEY = "gesture_raise_to_wake";
     protected static final String GESTURE_HAND_WAVE_KEY = "gesture_hand_wave";
     protected static final String GESTURE_POCKET_KEY = "gesture_pocket";
     protected static final String DOUBLE_TAP_KEY = "doze_trigger_doubletap";
@@ -113,6 +114,11 @@ public final class Utils {
             com.android.internal.R.bool.config_supportDoubleTapWake);
     }
 
+    protected static boolean isRaiseToWakeEnabled(Context context) {
+        return Settings.System.getInt(context.getContentResolver(),
+                Settings.System.CUSTOM_AMBIENT_RAISE_GESTURE, 0) != 0;
+    }
+
     protected static boolean tiltGestureEnabled(Context context) {
         return Settings.System.getInt(context.getContentResolver(),
                 Settings.System.CUSTOM_AMBIENT_TILT_GESTURE, 0) != 0;
@@ -144,6 +150,13 @@ public final class Utils {
     protected static boolean enablePickUp(boolean enable, Context context) {
         boolean enabled = Settings.System.putInt(context.getContentResolver(),
                 Settings.System.CUSTOM_AMBIENT_TILT_GESTURE, enable ? 1 : 0);
+        manageService(context);
+        return enabled;
+    }
+
+    protected static boolean enableRaiseToWake(boolean enable, Context context) {
+        boolean enabled = Settings.System.putInt(context.getContentResolver(),
+                Settings.System.CUSTOM_AMBIENT_RAISE_GESTURE, enable ? 1 : 0);
         manageService(context);
         return enabled;
     }

ResurrectionRemix/packages_apps_CustomDoze

Signed-off-by: Varun Date <date.varun123@gmail.com>

src/com/custom/ambient/display/DozeSettings.java
@@ -74,6 +74,7 @@ public class DozeSettings extends PreferenceActivity implements PreferenceFragme
         private SwitchPreference mAoDPreference;
         private SwitchPreference mAmbientDisplayPreference;
         private SwitchPreference mPickUpPreference;
+        private SwitchPreference mRaiseToWakePreference;
         private SwitchPreference mHandwavePreference;
         private SwitchPreference mPocketPreference;
         private SystemSettingSwitchPreference mDozeOnChargePreference;
@@ -115,6 +116,11 @@ public class DozeSettings extends PreferenceActivity implements PreferenceFragme
             mPickUpPreference.setChecked(Utils.tiltGestureEnabled(mContext));
             mPickUpPreference.setOnPreferenceChangeListener(this);
 
+            mRaiseToWakePreference =
+                (SwitchPreference) findPreference(Utils.GESTURE_RAISE_TO_WAKE_KEY);
+            mRaiseToWakePreference.setChecked(Utils.isRaiseToWakeEnabled(mContext));
+            mRaiseToWakePreference.setOnPreferenceChangeListener(this);
+
             mHandwavePreference =
                 (SwitchPreference) findPreference(Utils.GESTURE_HAND_WAVE_KEY);
             mHandwavePreference.setChecked(Utils.handwaveGestureEnabled(mContext));
@@ -129,6 +135,7 @@ public class DozeSettings extends PreferenceActivity implements PreferenceFragme
             if (!getResources().getBoolean(R.bool.has_tilt_sensor)) {
                 getPreferenceScreen().removePreference(mTiltCategory);
                 getPreferenceScreen().removePreference(mPickUpPreference);
+                mRaiseToWakePreference.setVisible(false);
             }
 
             mProximitySensorCategory =
@@ -161,6 +168,11 @@ public class DozeSettings extends PreferenceActivity implements PreferenceFragme
                 mPickUpPreference.setChecked(value);
                 Utils.enablePickUp(value, mContext);
                 return true;
+            } else if (Utils.GESTURE_RAISE_TO_WAKE_KEY.equals(key)) {
+                boolean value1 = (Boolean) newValue;
+                mRaiseToWakePreference.setChecked(value1);
+                Utils.enableRaiseToWake(value1, mContext);
+                return true;
             } else if (Utils.GESTURE_HAND_WAVE_KEY.equals(key)) {
                 mHandwavePreference.setChecked(value);
                 Utils.enableHandWave(value, mContext);
@@ -177,6 +189,7 @@ public class DozeSettings extends PreferenceActivity implements PreferenceFragme
             final boolean aodEnabled = Utils.isAoDEnabled(mContext);
             mAmbientDisplayPreference.setEnabled(!aodEnabled);
             mPickUpPreference.setEnabled(!aodEnabled);
+            mRaiseToWakePreference.setEnabled(!aodEnabled);
             mHandwavePreference.setEnabled(!aodEnabled);
             mPocketPreference.setEnabled(!aodEnabled);
             mDozeOnChargePreference.setEnabled(!aodEnabled);

src/com/custom/ambient/display/Utils.java
@@ -39,6 +39,7 @@ public final class Utils {
     protected static final String AOD_CHARGE_KEY = "doze_on_charge";
     protected static final String AMBIENT_DISPLAY_KEY = "ambient_display";
     protected static final String PICK_UP_KEY = "pick_up";
+    protected static final String GESTURE_RAISE_TO_WAKE_KEY = "custom_ambient_raise_gesture";
     protected static final String GESTURE_HAND_WAVE_KEY = "gesture_hand_wave";
     protected static final String GESTURE_POCKET_KEY = "gesture_pocket";
     protected static final String DOUBLE_TAP_KEY = "doze_trigger_doubletap";
@@ -104,6 +105,11 @@ public final class Utils {
             com.android.internal.R.bool.config_supportDoubleTapWake);
     }
 
+    protected static boolean isRaiseToWakeEnabled(Context context) {
+        return Settings.System.getInt(context.getContentResolver(),
+                Settings.System.CUSTOM_AMBIENT_RAISE_GESTURE, 0) != 0;
+    }
+
     protected static boolean tiltGestureEnabled(Context context) {
         return Settings.System.getInt(context.getContentResolver(),
                 Settings.System.CUSTOM_AMBIENT_TILT_GESTURE, 0) != 0;
@@ -129,6 +135,13 @@ public final class Utils {
     protected static boolean enablePickUp(boolean enable, Context context) {
         boolean enabled = Settings.System.putInt(context.getContentResolver(),
                 Settings.System.CUSTOM_AMBIENT_TILT_GESTURE, enable ? 1 : 0);
+        manageService(context);
+        return enabled;
+    }
+
+    protected static boolean enableRaiseToWake(boolean enable, Context context) {
+        boolean enabled = Settings.System.putInt(context.getContentResolver(),
+                Settings.System.CUSTOM_AMBIENT_RAISE_GESTURE, enable ? 1 : 0);
         manageService(context);
         return enabled;
     }
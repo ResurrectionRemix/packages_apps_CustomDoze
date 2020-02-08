501022cf1e4cd653b8c6c630776d5857ff4ebe96
Nauval Rizky <enuma.alrizky@gmail.com>
2020-02-08 22:15:58 +0530

    CustomDoze: Allow to wake the screen instead of pulsing
     * Highly inspired by iOS raise to wake
    
    Co-authored-by: Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
    Co-authored-by: Anushek Prasal <anushekprasal@gmail.com>
    Change-Id: I832fd03e9538a507ec9b3ee7cc384bd71d6efbdd
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- src/com/custom/ambient/display/DozeSettings.java
+++ src/com/custom/ambient/display/DozeSettings.java
@@ -74,6 +74,7 @@ public class DozeSettings extends PreferenceActivity implements PreferenceFragme
         private SwitchPreference mAoDPreference;
         private SwitchPreference mAmbientDisplayPreference;
         private SwitchPreference mPickUpPreference;
+        private SwitchPreference mRaiseToWakePreference;
         private SwitchPreference mHandwavePreference;
         private SwitchPreference mPocketPreference;
         private SystemSettingSwitchPreference mDozeOnChargePreference;
@@ -115,6 +116,11 @@ public class DozeSettings extends PreferenceActivity implements PreferenceFragme
             mPickUpPreference.setChecked(Utils.tiltGestureEnabled(mContext));
             mPickUpPreference.setOnPreferenceChangeListener(this);
 
+            mRaiseToWakePreference =
+                (SwitchPreference) findPreference(Utils.GESTURE_RAISE_TO_WAKE_KEY);
+            mRaiseToWakePreference.setChecked(Utils.isRaiseToWakeEnabled(mContext));
+            mRaiseToWakePreference.setOnPreferenceChangeListener(this);
+
             mHandwavePreference =
                 (SwitchPreference) findPreference(Utils.GESTURE_HAND_WAVE_KEY);
             mHandwavePreference.setChecked(Utils.handwaveGestureEnabled(mContext));
@@ -129,6 +135,7 @@ public class DozeSettings extends PreferenceActivity implements PreferenceFragme
             if (!getResources().getBoolean(R.bool.has_tilt_sensor)) {
                 getPreferenceScreen().removePreference(mTiltCategory);
                 getPreferenceScreen().removePreference(mPickUpPreference);
+                mRaiseToWakePreference.setVisible(false);
             }
 
             mProximitySensorCategory =
@@ -161,6 +168,11 @@ public class DozeSettings extends PreferenceActivity implements PreferenceFragme
                 mPickUpPreference.setChecked(value);
                 Utils.enablePickUp(value, mContext);
                 return true;
+            } else if (Utils.GESTURE_RAISE_TO_WAKE_KEY.equals(key)) {
+                boolean value1 = (Boolean) newValue;
+                mRaiseToWakePreference.setChecked(value1);
+                Utils.enableRaiseToWake(value1, mContext);
+                return true;
             } else if (Utils.GESTURE_HAND_WAVE_KEY.equals(key)) {
                 mHandwavePreference.setChecked(value);
                 Utils.enableHandWave(value, mContext);
@@ -177,6 +189,7 @@ public class DozeSettings extends PreferenceActivity implements PreferenceFragme
             final boolean aodEnabled = Utils.isAoDEnabled(mContext);
             mAmbientDisplayPreference.setEnabled(!aodEnabled);
             mPickUpPreference.setEnabled(!aodEnabled);
+            mRaiseToWakePreference.setEnabled(!aodEnabled);
             mHandwavePreference.setEnabled(!aodEnabled);
             mPocketPreference.setEnabled(!aodEnabled);
             mDozeOnChargePreference.setEnabled(!aodEnabled);
--- src/com/custom/ambient/display/TiltSensor.java
+++ src/com/custom/ambient/display/TiltSensor.java
@@ -39,6 +39,8 @@ public class TiltSensor implements SensorEventListener {
 
     private static final int SENSOR_WAKELOCK_DURATION = 200;
     private static final int MIN_PULSE_INTERVAL_MS = 2500;
+    private static final int MIN_WAKEUP_INTERVAL_MS = 1000;
+    private static final int WAKELOCK_TIMEOUT_MS = 300;
 
     private PowerManager mPowerManager;
     private SensorManager mSensorManager;
@@ -48,6 +50,7 @@ public class TiltSensor implements SensorEventListener {
 
     private boolean mTiltGestureEnabled;
     private boolean mIsGlanceGesture;
+    private WakeLock mWakeLock;
 
     private long mEntryTimestamp;
     private boolean mEnabled;
@@ -73,6 +76,7 @@ public class TiltSensor implements SensorEventListener {
                 mIsGlanceGesture = true;
             }
         }
+        mWakeLock = mPowerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, TAG);
         mExecutorService = Executors.newSingleThreadExecutor();
         mSensorWakeLock = mPowerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,
                 "SensorWakeLock");
@@ -80,6 +84,8 @@ public class TiltSensor implements SensorEventListener {
 
     @Override
     public void onSensorChanged(SensorEvent event) {
+        boolean isRaiseToWake = Utils.isRaiseToWakeEnabled(mContext);
+
         if (DEBUG) Log.d(TAG, "Got sensor event: " + event.values[0]);
 
         long delta = SystemClock.elapsedRealtime() - mEntryTimestamp;
@@ -90,7 +96,13 @@ public class TiltSensor implements SensorEventListener {
         }
 
         if (event.values[0] == 1) {
-            Utils.launchDozePulse(mContext);
+            if (isRaiseToWake) {
+                mWakeLock.acquire(WAKELOCK_TIMEOUT_MS);
+                mPowerManager.wakeUp(SystemClock.uptimeMillis(),
+                        PowerManager.WAKE_REASON_GESTURE, TAG);
+            } else {
+                Utils.launchDozePulse(mContext);
+            }
         }
     }
 
